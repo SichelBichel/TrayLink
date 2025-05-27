caf7222a2dc0640cdaa0825494b9e9d037b44161
SichelBichel <siegenthalerluca@LTAL24-026>
2025-05-27 09:14:27 +0200

    a

---
+++ TrayLink/LinkItem.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace TrayLink
+{
+    partial class LinkItem
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelType = new Label();
+            labelName = new Label();
+            buttonExecute = new Button();
+            SuspendLayout();
+            // 
+            // labelType
+            // 
+            labelType.AutoSize = true;
+            labelType.Location = new Point(26, 18);
+            labelType.Name = "labelType";
+            labelType.Size = new Size(41, 20);
+            labelType.TabIndex = 0;
+            labelType.Text = "TYPE";
+            // 
+            // labelName
+            // 
+            labelName.AutoSize = true;
+            labelName.Location = new Point(16, 58);
+            labelName.Name = "labelName";
+            labelName.Size = new Size(51, 20);
+            labelName.TabIndex = 1;
+            labelName.Text = "NAME";
+            // 
+            // buttonExecute
+            // 
+            buttonExecute.Location = new Point(159, 36);
+            buttonExecute.Name = "buttonExecute";
+            buttonExecute.Size = new Size(114, 42);
+            buttonExecute.TabIndex = 2;
+            buttonExecute.Text = "EXECUTE";
+            buttonExecute.UseVisualStyleBackColor = true;
+            // 
+            // LinkItem
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(buttonExecute);
+            Controls.Add(labelName);
+            Controls.Add(labelType);
+            Name = "LinkItem";
+            Size = new Size(305, 107);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label labelType;
+        private Label labelName;
+        private Button buttonExecute;
+    }
+}
--- TrayLink/TrayPopupForm.Designer.cs
+++ TrayLink/TrayPopupForm.Designer.cs
@@ -30,7 +30,11 @@
         {
             components = new System.ComponentModel.Container();
             TrayIcon = new NotifyIcon(components);
-            button1 = new Button();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            buttonReloadCFG = new Button();
+            buttonOpenCFG = new Button();
             SuspendLayout();
             // 
             // TrayIcon
@@ -38,30 +42,71 @@
             TrayIcon.Text = "TrayLink";
             TrayIcon.Visible = true;
             // 
-            // button1
+            // flowLayoutPanel1
             // 
-            button1.Location = new Point(125, 46);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += test;
+            flowLayoutPanel1.BorderStyle = BorderStyle.FixedSingle;
+            flowLayoutPanel1.Location = new Point(12, 166);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(298, 476);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 133);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(298, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 110);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Search:";
+            // 
+            // buttonReloadCFG
+            // 
+            buttonReloadCFG.Location = new Point(172, 56);
+            buttonReloadCFG.Name = "buttonReloadCFG";
+            buttonReloadCFG.Size = new Size(138, 42);
+            buttonReloadCFG.TabIndex = 3;
+            buttonReloadCFG.Text = "Reload";
+            buttonReloadCFG.UseVisualStyleBackColor = true;
+            // 
+            // buttonOpenCFG
+            // 
+            buttonOpenCFG.Location = new Point(12, 56);
+            buttonOpenCFG.Name = "buttonOpenCFG";
+            buttonOpenCFG.Size = new Size(133, 42);
+            buttonOpenCFG.TabIndex = 4;
+            buttonOpenCFG.Text = "Open";
+            buttonOpenCFG.UseVisualStyleBackColor = true;
             // 
             // TrayPopupForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(409, 563);
-            Controls.Add(button1);
+            ClientSize = new Size(322, 654);
+            Controls.Add(buttonOpenCFG);
+            Controls.Add(buttonReloadCFG);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(flowLayoutPanel1);
             Name = "TrayPopupForm";
             Text = "TrayLink";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private NotifyIcon TrayIcon;
-        private Button button1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private TextBox textBox1;
+        private Label label1;
+        private Button buttonReloadCFG;
+        private Button buttonOpenCFG;
     }
 }
